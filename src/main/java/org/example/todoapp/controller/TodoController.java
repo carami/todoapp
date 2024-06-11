f7386ca1a60a67547567b62114146ef1cb4ea3d1
carami <carami@nate.com>
2024-06-11 11:44:46 +0900

    todo추가

---
+++ src/main/java/org/example/todoapp/controller/TodoController.java
@@ -0,0 +1,44 @@
+package org.example.todoapp.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.example.todoapp.entity.Todo;
+import org.example.todoapp.service.TodoService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/todos")
+@RequiredArgsConstructor
+public class TodoController {
+    private final TodoService todoService;
+
+    @GetMapping
+    public List<Todo> getTodos(){
+        return todoService.getTodos();
+    }
+    @GetMapping("/{id}")
+    public Todo getTodo(@PathVariable("id") Long id){
+        return todoService.getTodo(id);
+    }
+    @PostMapping
+    public Todo addTodo(@RequestBody Todo todo){
+        return todoService.addTodo(todo.getTodo());
+    }
+    //put vs patch    --  수정    put - 자체가 바뀔때..  patch  --  부분만 수정될때
+    @PatchMapping("/{id}")
+    public Todo updateTodoById(@PathVariable("id") Long id){
+        return todoService.updateTodo(id);
+    }
+
+    @PatchMapping
+    public Todo updateTodo(@RequestBody Todo todo){
+        return todoService.updateTodo(todo.getId());
+    }
+    @DeleteMapping
+    public String deleteTodo(@RequestBody Todo todo){
+        todoService.deleteTodo(todo.getId());
+        return "ok";
+    }
+}
