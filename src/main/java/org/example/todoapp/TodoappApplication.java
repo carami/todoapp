331caf9fb1746590e54438a5e70b4524a0104c1a
carami <carami@nate.com>
2024-06-12 14:28:29 +0900

    update 파라메터스토어추가

--- src/main/java/org/example/todoapp/TodoappApplication.java
+++ src/main/java/org/example/todoapp/TodoappApplication.java
@@ -5,6 +5,10 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.ssm.SsmClient;
+import software.amazon.awssdk.services.ssm.model.GetParameterRequest;
+import software.amazon.awssdk.services.ssm.model.GetParameterResponse;
 
 @SpringBootApplication
 public class TodoappApplication {
@@ -24,4 +28,30 @@ public class TodoappApplication {
 //			System.out.println("DB_PASSWORD:::"+dbPassword);
 //		};
 //	}
+
+	@Bean
+	public CommandLineRunner run(){
+
+		return args -> {
+			String region = "ap-northeast-2";
+
+			SsmClient ssmClient = SsmClient.builder()
+					.region(Region.of(region))
+					.build();
+
+
+			System.out.println("todo_DB_USERNAME:::"+getParameterValue(ssmClient,"/todo/config/DB_USERNAME"));
+			System.out.println("todo_DB_PASSWORD:::"+getParameterValue(ssmClient,"/todo/config/DB_PASSWORD"));
+		};
+	}
+
+	private String getParameterValue(SsmClient ssmClient, String parameterName){
+		GetParameterRequest parameterRequest = GetParameterRequest.builder()
+				.name(parameterName)
+				.withDecryption(true)
+				.build();
+
+		GetParameterResponse parameterResponse = ssmClient.getParameter(parameterRequest);
+		return parameterResponse.parameter().value();
+	}
 }
